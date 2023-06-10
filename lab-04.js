9d53809ade2e524704839b79a2f564ec7702e0b8
arange4 <134808987+arange4@users.noreply.github.com>
2023-06-10 16:20:44 -0700

    Add files via upload

---
+++ lab-04.js
@@ -0,0 +1,40 @@
+// Require the Fastify framework and instantiate it
+const fastify = require("fastify")();
+// Handle GET verb for / route using Fastify
+// Note use of "chain" dot notation syntax
+fastify.get("/", (request, reply) => {
+console.log(request.body, request.params, request.query)
+ reply
+   .code(200)
+   .header("Content-Type", "text/text; charset=utf-8")
+   .send("<h1>Hello from Lab 4!</h1>");
+});
+fastify.get("name", (request, reply) => {
+    const {first, last} = request.query;
+    if(last !== undefined && first !== undefined){
+        const response = "<h1>Hello, "+ first + " " + last + " "+ "<h1>";
+        reply
+        .code(200)
+        .header("Content-Type", "text/html; charset-utf-8")
+        .send("response");
+    }
+    else{
+        console.log(request.query)
+        reply
+        .code(200)
+        .header("Content-Type", "text/text; charset=utf-8")
+        .send("<h1>Hello, Guest!<h1>");
+    }
+
+})
+// Start server and listen to requests using Fastify
+const listenIP = "localhost";
+const listenPort = 8080;
+fastify.listen(listenPort, listenIP, (err, address) => {
+ if (err) {
+   console.log(err);
+   process.exit(1);
+ }
+ console.log(`Server listening on ${address}`);
+});
+
